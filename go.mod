5b10db3e82ee913a2232a8d8ae90f91065667924
Your Name <you@example.com>
2025-01-14 05:29:59 +0000

    Saving after crash

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ccutch/congo
 
-go 1.23.2
+go 1.23.4
 
 require (
 	github.com/digitalocean/godo v1.131.0
@@ -8,21 +8,21 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/google/uuid v1.6.0
 	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/ollama/ollama v0.5.5
 	github.com/pkg/errors v0.9.1
 	github.com/sosedoff/gitkit v0.4.0
-	golang.org/x/crypto v0.27.0
+	golang.org/x/crypto v0.31.0
 	golang.org/x/oauth2 v0.23.0
 )
 
 require (
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 )
---
+++ pkg/congo_bots/congo-bots.go
@@ -0,0 +1,50 @@
+package congo_bots
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ccutch/congo/pkg/congo"
+	"github.com/ccutch/congo/pkg/congo_host"
+)
+
+type CongoBots struct {
+	DB     *congo.Database
+	client *congo_host.Service
+	models map[string]bool
+}
+
+func InitCongoBots(host congo_host.CongoHost, opts ...CongoBotsOpt) *CongoBots {
+	db := congo.SetupDatabase(host.DB.Root, "bots.db", nil)
+	if err := db.MigrateUp(); err != nil {
+		log.Fatal("Failed to setup bots db:", err)
+	}
+	service := host.Local().Service("ollama",
+		congo_host.WithImage("ollama/ollama"),
+		congo_host.WithTag("latest"),
+		congo_host.WithVolume(fmt.Sprintf("%s/services/ollama:/root/.ollama", host.DB.Root)))
+	go service.Start()
+	return &CongoBots{db, service, map[string]bool{}}
+}
+
+type CongoBotsOpt func(*CongoBots)
+
+func WithModel(name string) CongoBotsOpt {
+	return func(bots *CongoBots) {
+		bots.models[name] = false
+		go func() {
+			client, err := api.ClientFromEnvironment()
+			if err != nil {
+				log.Fatal("Failed to create ollama client:", err)
+			}
+
+			err = client.Pull(context.Background(), &api.PullRequest{Model: name}, nil)
+			if err != nil {
+				log.Fatal("Failed to pull ollama model:", err)
+			}
+
+			bots.models[name] = true
+		}()
+	}
+}
