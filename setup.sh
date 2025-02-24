9277705e1f109812c5e56332a8d6a7760fdf58cb
Congo User <help@congo.com>
2025-02-24 00:12:43 +0000

    updates golang

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg wget lsb-release tmux certbot build-essential npm
 
 echo Installing golang from source...
-wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && \
+wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz && \
     sudo rm -rf /usr/local/go && \
-    sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz && \
-rm go1.23.2.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.24.0.linux-amd64.tar.gz && \
+rm go1.24.0.linux-amd64.tar.gz
 
 echo Updating Bash environment
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
